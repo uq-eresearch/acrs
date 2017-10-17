6d48684a25e3db8dbae5f7a0f77c21fa50a5e0f9
Andre Gebers <a.gebers@uq.edu.au>
2017-10-17 17:06:18 +1000

    add docker build

---
+++ acrs-portlets/build_docker.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+ACRS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DOCKER_DIR=${1:-$ACRS_DIR/target/docker}
+rm -rf $DOCKER_DIR
+mkdir -p $DOCKER_DIR
+if [ ! -f $ACRS_DIR/target/acrs-portlets.war ]; then
+  mvn -f $ACRS_DIR/pom.xml clean package
+fi
+wget -P $DOCKER_DIR https://swift.rc.nectar.org.au:8888/v1/AUTH_ffc889d904964131af390cd202d8f603/docker/acrs-liferay.tar.gz
+cp $ACRS_DIR/Dockerfile $DOCKER_DIR
+cp $ACRS_DIR/target/acrs-portlets.war $DOCKER_DIR
+( cd $DOCKER_DIR && docker build -t acrs . )
