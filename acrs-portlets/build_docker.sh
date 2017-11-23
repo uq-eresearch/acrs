13d1bde45a747187e78e8460a803e8a2c068a8e2
Andre Gebers <a.gebers@uq.edu.au>
2017-11-23 15:49:55 +1000

    fixed image upload. liferay is now run as user liferay

--- acrs-portlets/build_docker.sh
+++ acrs-portlets/build_docker.sh
@@ -9,6 +9,8 @@ if [ ! -f $ACRS_DIR/target/acrs-portlets.war ]; then
   mvn -f $ACRS_DIR/pom.xml clean package
 fi
 wget -P $DOCKER_DIR https://swift.rc.nectar.org.au:8888/v1/AUTH_ffc889d904964131af390cd202d8f603/docker/acrs-liferay.tar.gz
+wget -P $DOCKER_DIR https://swift.rc.nectar.org.au:8888/v1/AUTH_ffc889d904964131af390cd202d8f603/docker/server-jre-8u152-linux-x64.tar.gz
+wget -P $DOCKER_DIR https://swift.rc.nectar.org.au:8888/v1/AUTH_ffc889d904964131af390cd202d8f603/docker/ecj-4.5.jar
 cp $ACRS_DIR/Dockerfile $DOCKER_DIR
 cp $ACRS_DIR/target/acrs-portlets.war $DOCKER_DIR
 ( cd $DOCKER_DIR && docker build -t acrs . )
